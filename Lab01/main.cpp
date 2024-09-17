18a32ff8018a763ab1ca9a1009928dffda3d59e2
Ihor Diuba <idyuba@gmail.com>
2024-09-17 23:19:55 +0300

    add Lab01 Shift register with feedback

---
+++ Lab01/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+
+
+// polynom (8, 4, 3, 2, 0)
+// reg cnt = 8
+// bits position 4, 3, 2, 0
+
+const size_t bitsCnt = 8;
+std::bitset<bitsCnt>  reg;
+std::vector<int> bitsPositions{ 4, 3, 2, 0 };
+
+void findCodeKey() {
+	bool xorValue = false;
+	for (const auto& bit : bitsPositions) {
+		xorValue ^= reg[bit];
+	}
+	reg >>= 1;
+	reg[bitsCnt - 1] = xorValue;
+}
+
+std::string codeText(const std::string& str) 
+{
+	std::string output;
+	for (const char& c : str) {
+		findCodeKey();
+		char codedSympol = c ^ static_cast<char>(reg.to_ullong());
+		output += codedSympol;
+	}
+	return output;
+}
+
+int main() {
+	
+	std::string initialText = "ABC def 123 !@#";
+	std::cout << "Initial text:  " << initialText << "\n";
+
+	reg.set(); // all bits 1
+	std::string codedText = codeText(initialText);
+	std::cout << "Coded text:    " << codedText << "\n";
+
+	reg.set(); // all bits 1
+	std::string deCodeText = codeText(codedText);
+	std::cout << "DeCoded text:  " << deCodeText << "\n";
+
+	return 0;
+}
